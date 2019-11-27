7bb977e1815ab5afc49f619c3cd7d183dbb62711
Frank Matranga <thefrankmatranga@gmail.com>
2019-11-27 16:13:40 -0500

    Model documentation

--- club/models.py
+++ club/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-### Events represent one-time club meetings. 
 class Event(models.Model):
+    '''Events represent one-time club meetings.'''
+
     # Recognized types of events
     # Taken from https://sites.google.com/google.com/developerstudentclubleads/dsc-activities-reporting
     EVENT_TYPES = [
@@ -15,7 +16,7 @@ class Event(models.Model):
         ('CT', 'Core Team Meeting')
     ]
 
-    event_type = models.CharField(max_length=2, choices=EVENT_TYPES)
+    event_type = models.CharField(max_length=2, choices=EVENT_TYPES, help_text='Whether the event is a Workshop, Info session, etc.')
     
     # Who can see the event
     VISIBILITY_TYPES = [
@@ -23,23 +24,23 @@ class Event(models.Model):
         ('M', 'Members'),
         ('C', 'Core Team')
     ]
-    visibility = models.CharField(max_length=1, choices=VISIBILITY_TYPES)
+    visibility = models.CharField(max_length=1, choices=VISIBILITY_TYPES, help_text='Determines who can see/register for the event.')
     
     # Title of Event limited to 100 characters
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, help_text='The headline title of the event. Should be very concise.')
     
     # (Optional) Event tagline
-    tagline = models.CharField(max_length=200, blank=True, null=True)
+    tagline = models.CharField(max_length=200, blank=True, null=True, help_text='An optional tagline that will be displayed with the event title if it exists.')
     
     # Required event description
-    description = models.CharField(max_length=5000)
+    description = models.CharField(max_length=5000, help_text='Long descriptionf of the event. Supports Markdown.')
 
     # Date and time range of event
-    start = models.DateTimeField()
-    end = models.DateTimeField()
+    start = models.DateTimeField(help_text='The exact start date and time of the event.')
+    end = models.DateTimeField(help_text='The exact end date and time of the event.')
 
     # (optional) Link to slideshow
-    presentation_link = models.URLField(blank=True, null=True)
+    presentation_link = models.URLField(blank=True, null=True, help_text='An optional link to presentation slides for the event. Most likely Google Slides.')
 
     # Timestamps
     created_at = models.DateTimeField(auto_now_add=True)
@@ -50,17 +51,17 @@ class Event(models.Model):
     def __str__(self):
         return f'{self.title}: {self.get_event_type_display()} on {self.start.strftime("%m/%d/%Y")}'
 
-### Project represents a "tech based solution"
 class Project(models.Model):
+    '''Project represents a tech-based solution.'''
 
     # Title of the project
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, help_text='Required title for project.')
     
     # Description of the project
-    description = models.CharField(max_length=3000)
+    description = models.CharField(max_length=3000, help_text='Long description of the project.')
 
     # Link to project code
-    repository_link = models.URLField()
+    repository_link = models.URLField(help_text='Link to where the code can be found. Most likely GitHub.')
 
     def __str__(self):
         return f'Project "{self.title}" ({self.repository_link})'
