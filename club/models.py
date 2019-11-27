63f73af7c41c157b33835847f85efd6b0ae5250b
Frank Matranga <thefrankmatranga@gmail.com>
2019-11-27 13:25:49 -0500

    Add Event model

--- club/models.py
+++ club/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+### Events represent one-time club meetings. 
+class Event(models.Model):
+    # Recognized types of events
+    EVENT_TYPES = [
+        ('IS', 'Info Session'),
+        ('WS', 'Workshop'),
+        ('SJ', 'Study Jam Workshop'),
+        ('HO', 'Hands-on Workshop'),
+        ('HA', 'Hackathon'),
+        ('SS', 'Speaker Session'),
+        ('SC', 'Showcase')
+    ]
+    event_type = models.CharField(max_length=2, choices=EVENT_TYPES)
+    
+    # Title of Event limited to 100 characters
+    title = models.CharField(max_length=100)
+    
+    # Optional event tagline
+    tagline = models.CharField(max_length=200, null=True)
+    
+    # Required event description
+    description = models.CharField(max_length=5000)
+
+    # Date and time range of event
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
