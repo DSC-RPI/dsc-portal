9196c96bba4b0038424d3212365ca2ce11b56401
Frank Matranga <thefrankmatranga@gmail.com>
2019-11-27 16:20:58 -0500

    Basic event views and templates

--- club/urls.py
+++ club/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('events/', views.event_index),
+    path('events/<int:event_id>', views.event_detail)
 ]
--- club/views.py
+++ club/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-
+from .models import Event
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, 'club/index.html')
+
+
+# EVENTS
+def event_index(request):
+    events = Event.objects.all()
+    return render(request, 'club/events/index.html', {'events':events})
+
+def event_detail(request, event_id):
+    '''
+    Display an individual :model:`club.Event`.
+
+    **Context**
+
+    ``event``
+        An instance of :model:`club.Event`.
+
+    **Template:**
+
+    :template:`club/event_detail.html`
+    '''
+    event = get_object_or_404(Event, pk=event_id)
+    return render(request, 'club/events/detail.html', {'event':event})
