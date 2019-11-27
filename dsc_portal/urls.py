5e2591ea3a1c4533c645b6521ad8a41544786609
Frank Matranga <thefrankmatranga@gmail.com>
2019-11-27 16:17:53 -0500

    Create base template

--- dsc_portal/urls.py
+++ dsc_portal/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('club.urls')),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
+    path('admin/', admin.site.urls),
 ]
