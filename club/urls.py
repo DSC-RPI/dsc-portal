bf7296401598660b44d8923787a2600f346f9a81
Frank Matranga <thefrankmatranga@gmail.com>
2019-12-31 19:17:45 -0500

    Refactor roadmap

--- club/urls.py
+++ club/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('members/', views.member_index, name='members'),
     path('members/<int:member_id>', views.member_detail, name='member_detail'),
     path('core-team/', views.core_team, name='core_team'),
-    path('core-team/roadmap', views.roadmap_index, name='roadmap_index'),
+    path('core-team/roadmap', views.roadmap_index, name='roadmap'),
     path('core-team/social-media', views.social_media, name='social_media'),
     path('account/', views.user_account, name='user_account')
 ]
