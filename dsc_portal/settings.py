d475378ccb2cba26d5aabd42d2f4be912a585e73
Frank Matranga <thefrankmatranga@gmail.com>
2020-01-07 18:44:02 -0500

    Prod settings

--- dsc_portal/settings.py
+++ dsc_portal/settings.py
@@ -113,11 +113,11 @@ DATABASES = {
 }
 
 if os.environ['ENV'] != 'development':
-    # SECURE_CONTENT_TYPE_NOSNIFF = True
-    # SECURE_BROWSER_XSS_FILTER = True
-    # SECURE_SSL_REDIRECT = True
-    # CSRF_COOKIE_SECURE = True
-    # SESSION_COOKIE_SECURE = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_SSL_REDIRECT = True
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
     X_FRAME_OPTIONS = 'DENY'
 
 # Password validation
