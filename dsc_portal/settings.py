141142bb693f8138fd3b2bd8ed71ae6a38ada3c0
Frank Matranga <thefrankmatranga@gmail.com>
2020-01-08 13:01:32 -0500

    Oops

--- dsc_portal/settings.py
+++ dsc_portal/settings.py
@@ -14,6 +14,7 @@ import os
 import json
 from google.oauth2 import service_account
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
