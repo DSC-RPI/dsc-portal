a07235260323ac33f4d6bae3b6b7d02e4f9362ad
Frank Matranga <thefrankmatranga@gmail.com>
2020-01-10 01:30:42 -0500

    Send templated email

--- club/email.py
+++ club/email.py
@@ -1,5 +1,20 @@
 from django.core.mail import send_mail as sm
+from django.template.loader import render_to_string
 from django.conf import settings
+
+FROM = f'DSC {settings.SCHOOL_NAME_SHORT} <{settings.GOOGLE_ACCOUNT}>'
+
+def send_templated_email(subject, template, data, recipients):
+    # https://stackoverflow.com/questions/2809547/creating-email-templates-with-django
+    html_message = render_to_string(f'club/emails/{template}.html', data)
+    try:
+        # Try to find the plaintext template
+        plain_message = render_to_string(f'club/emails/{template}.txt', data)
+    except:
+        # If it doesn't exist, fallback to just HTML
+        plain_message = html_message
+
+    return sm(subject, plain_message, FROM, recipients, html_message=html_message)
 
 def send_email(subject, body, recipients):
     '''
@@ -9,4 +24,4 @@ def send_email(subject, body, recipients):
     body: str
     recipients: list
     '''
-    return sm(subject, body, f'DSC {settings.SCHOOL_NAME_SHORT} <{settings.GOOGLE_ACCOUNT}>', recipients)
+    return sm(subject, body, FROM, recipients, html_message=body)
