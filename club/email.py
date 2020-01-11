727ac9b6c1002bbc70a6356b51fc873391d66ec4
Frank Matranga <thefrankmatranga@gmail.com>
2020-01-11 13:25:34 -0500

    Html to text for plain emails

--- club/email.py
+++ club/email.py
@@ -2,6 +2,7 @@ from django.core.mail import send_mail as sm
 from django.template.loader import render_to_string
 from django.conf import settings
 from .logger import logger
+from html2text import html2text
 
 FROM = f'DSC {settings.SCHOOL_NAME_SHORT} <{settings.GOOGLE_ACCOUNT}>'
 
@@ -13,7 +14,7 @@ def send_templated_email(subject, template, data, recipients):
         plain_message = render_to_string(f'club/emails/{template}.txt', data)
     except:
         # If it doesn't exist, fallback to just HTML
-        plain_message = html_message
+        plain_message = html2text(html_message)
 
     logger.info(f'Sending email with subject "{subject}" to {recipients} with template "{template}"')
     return sm(subject, plain_message, FROM, recipients, html_message=html_message)
@@ -27,4 +28,4 @@ def send_email(subject, body, recipients):
     recipients: list
     '''
     logger.info(f'Sending email with subject "{subject}" to {recipients}')
-    return sm(subject, body, FROM, recipients, html_message=body)
+    return sm(subject, html2text(body), FROM, recipients, html_message=body)
